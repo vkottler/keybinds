7e260a40dcdebae72c8df22d3d3dc80bc94d8214
vkottler <vaughnkottler@gmail.com>
2020-10-04 15:36:25 -0700

    learning some structopt!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+/// A tool for interacting with keybind manifests.
+struct Opt {
+    #[structopt(parse(from_os_str))]
+    /// Path to the manifest configuration.
+    manifest: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+
+    println!("{:?}", opt.manifest.as_path());
 }
