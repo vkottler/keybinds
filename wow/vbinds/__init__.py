08d6801b3b6012315d75e8176c9b188547840257
Vaughn Kottler <vaughnkottler@gmail.com>
2020-12-06 17:01:23 -0800

    use datazen to template certain things

--- wow/vbinds/__init__.py
+++ wow/vbinds/__init__.py
@@ -1,7 +1,8 @@
 
 """
-vbinds - Package constants.
+vbinds - Useful defaults and other package metadata.
 """
 
+PKG_NAME = "blizzard-api"
+VERSION = "0.2.1"
 DESCRIPTION = "Interact with Blizard's game-data API."
-VERSION = "0.2.0"
