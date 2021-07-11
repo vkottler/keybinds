a97fd34985c99d10156300dcc735c898364520cc
Vaughn Kottler <vaughnkottler@gmail.com>
2021-07-11 14:15:32 -0700

    0.3.3 - update package deploy

--- wow/vbinds/__init__.py
+++ wow/vbinds/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=1.7.8
-# hash=b47c3c5371ed91925b6597de932a51e8
+# hash=50258eb10fdedcc64d652281e09b844c
 # =====================================
 
 """
@@ -9,5 +9,5 @@ vbinds - Useful defaults and other package metadata.
 """
 
 PKG_NAME = "blizzard-api"
-VERSION = "0.3.2"
+VERSION = "0.3.3"
 DESCRIPTION = "Interact with Blizard's game-data API."
